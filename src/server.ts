000b336ebb65fa3f38f8a885ddb63af24eeb2cc7
sayro <sayrodamaceno70@gmail.com>
2023-05-19 20:43:08 -0300

    feat: config access token

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,8 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import { memoriesRoutes } from './routes/memories'
+import { authRoutes } from './routes/auth'
+import jwt from '@fastify/jwt'
 
 const app = fastify()
 
@@ -10,6 +12,11 @@ app.register(cors, {
   origin: true,
 })
 
+app.register(jwt, {
+  secret: 'spacetime',
+})
+
+app.register(authRoutes)
 app.register(memoriesRoutes)
 
 app
